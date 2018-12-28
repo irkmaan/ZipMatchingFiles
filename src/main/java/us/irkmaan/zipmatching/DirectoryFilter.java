3eeacc07979c38da7784582dafa5ab47cbe4e743
irkmaan <ccjpotter000@gmail.com>
2018-12-28 10:07:16 -0500

    Create DirectoryFilter.java

---
+++ src/main/java/us/irkmaan/zipmatching/DirectoryFilter.java
@@ -0,0 +1,14 @@
+package us.irkmaan.zipmatching;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class DirectoryFilter implements FileFilter {
+
+	@Override
+	public boolean accept(File arg0) 
+	{
+		return arg0.isDirectory();
+	}
+
+}
